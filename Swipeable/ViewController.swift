9eb88cee37205369cb051c2d04ce5ab3363be0fc
Kevin Bluer <kevin@bluer.com>
2014-09-09 22:29:58 +0800

    init

---
+++ Swipeable/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Swipeable
+//
+//  Created by Kevin Bluer on 9/9/14.
+//  Copyright (c) 2014 Bluer Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+
+}
+
